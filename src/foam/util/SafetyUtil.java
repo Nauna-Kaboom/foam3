ebae54691e1e472bfda7684628e5131f0ebda680
Nauna <anna.doulatshahi@gmail.com>
2023-05-07 07:02:09 -0400

    bit of clean house & SafetyUtil.isEmpty(String[])

--- src/foam/util/SafetyUtil.java
+++ src/foam/util/SafetyUtil.java
@@ -354,6 +354,10 @@ public class SafetyUtil {
     }
   }
 
+  public static <T> boolean isEmpty(T[] s) {
+    return s == null || s.length == 0;
+  }
+
   public static boolean isEmpty(String s) {
     return s == null || s.trim().isEmpty();
   }
