ce168133e1d1dea399c169a2dd06dc11ca30d957
Nauna <anna.doulatshahi@gmail.com>
2025-02-28 20:07:49 -0500

    asdf

--- src/foam/u2/view/LoginView.js
+++ src/foam/u2/view/LoginView.js
@@ -185,7 +185,7 @@ foam.CLASS({
     padding: 20px;
     margin: 0 0 12vh 0;
     box-shadow: inset 0px -2px 4px 1px black;
-    background: white;
+    background-color: /*%PRIMARY3%*/ #a6b4aa;
     border-radius: /*%INPUTBORDERRADIUS%*/ $inputBorderRadius;
     color: black;
     border: 2px ridge /*%SECONDARY3%*/ red;
